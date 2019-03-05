4b9dae15e9f0f8ef787819cc813df725f5ec336e
Howard Ding <hding2@illinois.edu>
2019-03-05 15:25:19 -0600

    Fixed class name

--- test/util/time_utils_test.rb
+++ test/util/time_utils_test.rb
@@ -6,23 +6,23 @@ class TimeUtilsTest < ActiveSupport::TestCase
 
   test 'eta works' do
     expected = 1.year.from_now
-    actual = TimeUtil.eta(5.hours.ago, 0.0)
+    actual = TimeUtils.eta(5.hours.ago, 0.0)
     assert actual - expected < 1
 
     expected = 5.hours.from_now
-    actual = TimeUtil.eta(5.hours.ago, 0.5)
+    actual = TimeUtils.eta(5.hours.ago, 0.5)
     assert actual - expected < 1
 
     expected = 6.hours.from_now
-    actual = TimeUtil.eta(2.hours.ago, 0.25)
+    actual = TimeUtils.eta(2.hours.ago, 0.25)
     assert actual - expected < 1
 
     expected = 2.hours.from_now
-    actual = TimeUtil.eta(6.hours.ago, 0.75)
+    actual = TimeUtils.eta(6.hours.ago, 0.75)
     assert actual - expected < 1
 
     expected = Time.now.utc
-    actual = TimeUtil.eta(6.hours.ago, 1.0)
+    actual = TimeUtils.eta(6.hours.ago, 1.0)
     assert actual - expected < 1
   end
 
