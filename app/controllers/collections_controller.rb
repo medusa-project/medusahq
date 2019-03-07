05ac956465513f4e498f06b4c1ca4dbe98b7c671
Howard Ding <hding2@illinois.edu>
2019-03-07 15:59:18 -0600

    Add the rest of the fields that are currently shown.

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -12,7 +12,7 @@ class CollectionsController < ApplicationController
     @limit = params[:limit]&.to_i || DEFAULT_WINDOW_SIZE
 
     @collections = Collection.order(:title)
-    @count       = @collections.count
+    @count = @collections.count
     @collections = @collections.
         offset(@start).
         limit(@limit)
@@ -30,13 +30,23 @@ class CollectionsController < ApplicationController
   end
 
   def update
-
+    if @collection.update_attributes(allowed_params)
+      redirect_to @collection
+    else
+      render 'edit'
+    end
   end
 
   private
 
   def get_collection
     @collection = Collection.find_by(uuid: params[:uuid])
+  end
+
+  def allowed_params
+    params[:collection].permit(:title, :description, :access_url, :physical_collection_url, :external_id,
+                               :representative_image_id, :representative_item_id,
+                               :private_description, :notes)
   end
 
 end
