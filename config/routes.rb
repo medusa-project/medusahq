e1a5e1f553569409e1d5fe144562ade97e54157d
Howard Ding <hding2@illinois.edu>
2019-03-22 10:50:12 -0500

    Add some expected routes for auth

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   root 'landing#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :collections, param: :uuid
+
+  #auth routes
+  match '/auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
+  match '/login', to: 'sessions#new', as: :login, via: [:get, :post]
+  match '/logout', to: 'sessions#destroy', as: :logout, via: [:get, :post]
+
 end
