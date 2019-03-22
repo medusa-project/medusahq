229a82a5f47df0e34262a0f41e2aa9790b574fbe
Howard Ding <hding2@illinois.edu>
2019-03-22 10:04:24 -0500

    Add initial shot at omniauth config, based on the CR config

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,10 @@
+require_relative 'config'
+Rails.application.config.middleware.use OmniAuth::Builder do
+  if Rails.env.production?
+    opts = Settings.shibboleth
+    provider :shibboleth, opts.to_h.symbolize_keys
+  else
+    provider :developer
+  end
+end
+OmniAuth.config.logger = Rails.logger
