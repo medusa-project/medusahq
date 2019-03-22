cb1034ab4efe084bc5b7410fc36858d0f4ef79df
Howard Ding <hding2@illinois.edu>
2019-03-22 14:40:50 -0500

    Require login to access anything except auth stuff. At some point we may want to allow basic_auth for machine clients as well.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
 
   helper_method :current_user
+  before_action :require_logged_in
 
   def current_user
     @current_user ||= User.find_by(id: session[:current_user_id])
@@ -14,6 +15,19 @@ class ApplicationController < ActionController::Base
   def unset_current_user
     @current_user = nil
     session[:current_user_id] = nil
+  end
+
+  def require_logged_in
+    redirect_non_logged_in_user unless logged_in?
+  end
+
+  def logged_in?
+    current_user.present?
+  end
+
+  def redirect_non_logged_in_user
+    session[:login_return_uri] = request.env['REQUEST_URO']
+    redirect_to(login_path)
   end
 
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,7 @@
 class SessionsController < ApplicationController
 
   skip_before_action :verify_authenticity_token
+  skip_before_action :require_logged_in
 
   def new
     set_return_url
