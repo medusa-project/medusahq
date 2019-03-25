0472a7e8c6819a1f7d2b576f8c41902aee7777ff
Howard Ding <hding2@illinois.edu>
2019-03-25 09:21:56 -0500

    Add repositories link to main navbar and replace collections link in breadcrumbs with owning repository for collection view.

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -8,6 +8,8 @@ class Collection < ApplicationRecord
   validates_presence_of :title
   #TODO optional: true is merely to make the existing tests pass - we'll probably want to remove it later
   belongs_to :repository, primary_key: :uuid, foreign_key: :repository_uuid, optional: true
+  delegate :title, to: :repository, prefix: true
+
 
   def to_param
     self.uuid
