2d37731614d31fa7cd142f23b2b133f5729b4e23
Howard Ding <hding2@illinois.edu>
2019-06-03 09:26:16 -0500

    Adding virtual repository functionality

--- app/controllers/virtual_repositories_controller.rb
+++ app/controllers/virtual_repositories_controller.rb
@@ -1,7 +1,19 @@
 class VirtualRepositoriesController < ApplicationController
 
+  before_action :get_virtual_repository, only: %i(show edit update destroy)
+
   def index
     @virtual_repositories = VirtualRepository.order(:title).all
+  end
+
+  def show
+
+  end
+
+  private
+
+  def get_virtual_repository
+    @virtual_repository = VirtualRepository.find(params[:id])
   end
 
 end
